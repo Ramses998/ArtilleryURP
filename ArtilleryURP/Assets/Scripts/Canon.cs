92458a513716e09be0d23cd9346451d58cb093d0
Ramses Figueroa <ramsesfig98@live.com>
2023-01-04 15:12:36 -0800

    Práctica-Módulo-17
    Práctica-Módulo-17

--- ArtilleryURP/Assets/Scripts/Canon.cs
+++ ArtilleryURP/Assets/Scripts/Canon.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class Canon : MonoBehaviour
 {
+    public static bool Bloqueado;
+
+    public AudioClip clipDisparo;
+    private GameObject SonidoDisparo;
+    private AudioSource SourceDisparo;
+
     [SerializeField] private GameObject BalaPrefab;
+    public GameObject ParticulasDisparo;
     private GameObject puntaCanon;
     private float rotacion;
 
     private void Start()
     {
         puntaCanon = transform.Find("PuntaCanon").gameObject;
+        SonidoDisparo = GameObject.Find("SonidoDisparo");
+        SourceDisparo = SonidoDisparo.GetComponent<AudioSource>();
     }
 
     void Update()
     {
         rotacion += Input.GetAxis("Horizontal") * AdministradorJuego.VelocidadRotacion;
-        if (rotacion <= 90 && rotacion >= 0) {
+        if (rotacion <= 90 && !Bloqueado) {
             transform.eulerAngles = new Vector3(rotacion, 90, 0.0f);
         }
 
         if (rotacion > 90) rotacion = 90;
         if (rotacion < 0) rotacion = 0;
 
-        if (Input.GetKeyDown(KeyCode.Space) && AdministradorJuego.DisparosPorJuego >0) {
+        if (Input.GetKeyDown(KeyCode.Space) && !Bloqueado) {
             GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+
             Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+            SeguirCamara.objetivo = temp;  //Aquí se define que la bala instanciada es el objetivo.
             Vector3 direccionDisparo = transform.rotation.eulerAngles;
             direccionDisparo.y = 90 - direccionDisparo.x;
+            Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+            GameObject Particulas = Instantiate
+                (ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
             tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.VelocidadBala;
-            AdministradorJuego.DisparosPorJuego--;
+            //AdministradorJuego.DisparosPorJuego--;
+            SourceDisparo.PlayOneShot(clipDisparo);
+            Bloqueado = true;
         }
     }
 }
